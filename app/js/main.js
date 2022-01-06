549222886d33f13b6dbe06372aa18f3df321d10a
magaFrelancer <ban0le4@gmail.com>
2022-01-06 22:42:28 +0300

    portfolio-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-
+  
+    const bodyStyles = window.getComputedStyle(document.body);
+    const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap')); //get
+    const portfolioSlider = new Swiper('.portfolio-section__items', {
+        slidesPerView: 3,
+        spaceBetween: gap,
+        loop: true,
+        navigation: {
+            nextEl: '.portfolio-section__next',
+            prevEl: '.portfolio-section__prev',
+        },
+    });
 })
