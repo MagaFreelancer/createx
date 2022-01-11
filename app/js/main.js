27a54c30606972864ab6b368bd463cc330c0a140
magaFrelancer <ban0le4@gmail.com>
2022-01-11 12:52:14 +0300

    partners

--- app/js/main.js
+++ app/js/main.js
@@ -11,4 +11,13 @@ $(function () {
             prevEl: '.portfolio-section__prev',
         },
     });
+
+    const testimonialsSlider = new Swiper('.testimonials__items', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+    });
 })
