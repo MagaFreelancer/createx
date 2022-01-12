03739359c563327ce33fd29064190655d75e5357
magaFrelancer <ban0le4@gmail.com>
2022-01-12 16:40:49 +0300

    facts

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  
+
     const bodyStyles = window.getComputedStyle(document.body);
     const gap = parseInt(bodyStyles.getPropertyValue('--grid-gap')); //get
     const portfolioSlider = new Swiper('.portfolio-section__items', {
@@ -20,4 +20,52 @@ $(function () {
             prevEl: '.testimonials__prev',
         },
     });
+
+
+    // const circleOne = document.querySelector('.facts-element__circle--one-color .progress');
+    // const circleTwo = document.querySelector('.facts-element__circle--two-color .progress');
+    // const circleThree = document.querySelector('.facts-element__circle--three-color .progress');
+    // const circleFour = document.querySelector('.facts-element__circle--four-color .progress');
+
+    // const progressAnimation = (circleItem, circlePrecent) => {
+    //     let percentageProgress = circlePrecent;
+
+    //     let radius = circleItem.getAttribute('r');
+    //     let circleLength = 2 * Math.PI * radius;
+
+    //     circleItem.setAttribute('stroke-dasharray', circleLength);
+    //     circleItem.setAttribute('stroke-dashoffset', circleLength - circleLength * percentageProgress / 100);
+    // }
+    // progressAnimation(circleTwo, 70);
+    // progressAnimation(circleOne, 80);
+    // progressAnimation(circleThree, 40);
+    // progressAnimation(circleFour, 56);
+    const circles = document.querySelectorAll(".facts-element__circle");
+
+    circles.forEach(el => {
+
+
+        if (el.dataset.percentage) {
+            let progress = el.querySelector('.progress');
+            let valueBlock = el.querySelector('.facts-element__value');
+            let radius = progress.getAttribute('r');
+            let circleLength = 2 * Math.PI * radius;
+            let full = el.dataset.full;
+            let value = el.dataset.value;
+            let perecentageProgress = Math.floor(value / full * 100);
+            valueBlock.textContent = value
+            progress.setAttribute('stroke-dasharray', circleLength);
+            progress.setAttribute('stroke-dashoffset', circleLength - circleLength * perecentageProgress / 100);
+        } else {
+            let progress = el.querySelector('.progress');
+            let value = el.querySelector('.facts-element__value');
+            let radius = progress.getAttribute('r');
+            let circleLength = 2 * Math.PI * radius;
+            let percent = el.dataset.percent;
+            let perecentageProgress = Math.floor(percent);
+            value.textContent = percent + '%'
+            progress.setAttribute('stroke-dasharray', circleLength);
+            progress.setAttribute('stroke-dashoffset', circleLength - circleLength * perecentageProgress / 100);
+        }
+    })
 })
